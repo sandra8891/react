a7a28abf435bc12983eb9ee2684959c02d45d6dc
sandra8891 <sandraks8891@gmail.com>
2025-04-23 15:43:30 +0530

    update,delete

--- function_comp/src/component/Home.jsx
+++ function_comp/src/component/Home.jsx
@@ -20,11 +20,27 @@ export default function Home() {
         setCurrentTask(task);
     }
 
+    const updateTask=(id,updateTask)=>{
+        setEditing(false)
+        axios.put(`http://127.0.0.1:8000/api/tasks/${id}/`,updateTask)
+        .then(response=>{
+            setTask(task.map(task=>(task.id == id ? response.data :task)))
+        })
+        .catch(error=>console.log(error))
+    }
+    const deleteTask=(id)=>{
+        axios.delete(`http://127.0.0.1:8000/api/tasks/${id}/`)
+        .then(response=>{
+            setTask(task.filter(task=>task.id!==id))
+        })
+        .catch(error=>console.log(error))
+    }
+
     return (
         <div>
             <Nav />
             <h1>Todo</h1>
-          {editing ? <EditTASKForm currentTask={currentTask} /> : null}
+          {editing ? <EditTASKForm currentTask={currentTask}updateTask={updateTask} /> : null}
             <table className="table">
                 <thead>
                     <tr> 
@@ -41,8 +57,8 @@ export default function Home() {
                             <td>{todo.title}</td>
                             <td>{todo.description}</td>
                             <td>{todo.completed ? 'Completed' : 'Not completed'}</td> 
-                            <td><button onClick={()=>editTask(task)}>Edit</button></td>
-                            <td><button>Delete</button></td>
+                            <td><button onClick={()=>editTask(todo)}>Edit</button></td>
+                            <td><button onClick={()=>deleteTask(todo.id)}>Delete</button></td>
                         </tr>
                     ))}
                 </tbody>
@@ -50,17 +66,29 @@ export default function Home() {
         </div>
     );
 }
-
-const EditTASKForm = (currentTask) => {
+const EditTASKForm = ({currentTask,updateTask})=> {
     const [task,setTask] = useState(currentTask);
     console.log(task);
 
-    return(
+const handleChange=(e)=>{
+    const{name,value}=e.target;
+    setTask({...task,[name]:value})
+}
+
+
+const handleSubmit=(e)=>{
+    e.preventDefault();
+    updateTask(task.id,task)
+}
+
+
+
+     return(
         <div>
             <h1>Edit Task</h1>
-            <form action="">
-            Title <input type="text"  id="" className="form-control" />
-                Description <input type="text" id="" className="form-control" />
+            <form action="" onSubmit={handleSubmit}>
+            Title <input type="text"   value={task.title} name='title' onChange={handleChange} id="" className="form-control" />
+                Description <input type="text" id=""  value={task.description} onChange={handleChange} name='description' className="form-control" />
                 <button className="btn btn-success">Update</button>
             </form>
         </div>
